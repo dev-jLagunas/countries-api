aeffd326f64435698d8e3c3d173b1565b0f3573d
Juan Lagunas <dev.jlagunas@gmail.com>
2024-03-13 08:42:48 +0900

    Extend Tailwind config with custom colors and width.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["src/app/app.component.html"],
+  darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "dark-blue": "hsl(209, 23%, 22%)",
+        "very-dark-blue-dark": "hsl(207, 26%, 17%)",
+        "very-dark-blue-light": "hsl(200, 15%, 8%)",
+        "dark-gray": "hsl(0, 0%, 52%)",
+        "very-light-gray": "hsl(0, 0%, 98%)",
+      },
+      width: {
+        "90vw": "90vw",
+      },
+    },
   },
   plugins: [],
 };
