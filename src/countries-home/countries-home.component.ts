4b3eb39bfd1505c275169dc4b70feceb0b5731d5
Juan Lagunas <dev.jlagunas@gmail.com>
2024-03-13 08:53:17 +0900

    Create project components and service

---
+++ src/countries-home/countries-home.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-countries-home',
+  standalone: true,
+  imports: [],
+  templateUrl: './countries-home.component.html',
+  styleUrl: './countries-home.component.scss'
+})
+export class CountriesHomeComponent {
+
+}
---
+++ src/countries-home/country-single/country-single.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-country-single',
+  standalone: true,
+  imports: [],
+  templateUrl: './country-single.component.html',
+  styleUrl: './country-single.component.scss'
+})
+export class CountrySingleComponent {
+
+}
